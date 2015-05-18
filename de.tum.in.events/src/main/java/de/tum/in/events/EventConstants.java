4540e85083df50d8461d490bb16ccdfc09e485cf
Amit Kumar Mondal <admin@amitinside.com>
2015-05-18 07:17:13 +0200

    Added OSGi Event Constants Specific Bundle

---
+++ de.tum.in.events/src/main/java/de/tum/in/events/EventConstants.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (C) 2015 - Amit Kumar Mondal <admin@amitinside.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+package de.tum.in.events;
+
+/**
+ * All the system event constants should be registered here
+ * 
+ * @author AMIT KUMAR MONDAL
+ *
+ */
+public interface EventConstants {
+
+	/**
+	 * Used to cache data retrieved from the bluetooth enabled milling machine
+	 */
+	public static final String MILLING_MACHINE_DATA_CACHE = "de/tum/in/bluetooth/devices/milling/machine";
+
+}
