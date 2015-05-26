0e3757d03eb656c1d8892be6ca17e05e2fc4717b
Amit Kumar Mondal <admin@amitinside.com>
2015-05-27 01:25:49 +0200

    Added Deployment Package Parent POM Added Automation Script to Generate Deployment Package on the fly

---
+++ de.tum.in.dp.parent/generate-dp.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+declare -a arr=("de.tum.in.dp.bluetooth" "de.tum.in.dp.bluetooth.machine" "de.tum.in.dp.cache" "de.tum.in.dp.commons" "de.tum.in.dp.dependencies")
+b="_build.xml"
+c="/"
+home_dir="/Users/AMIT/IoT_IDP/Pi/"
+
+for i in "${arr[@]}"
+do
+   echo "Generating Deployment Package for $i"
+   ant -buildfile  $home_dir$i$c$i$b
+done
